88c942e5cc38898f6b4b8fc2f708ee7a07486518
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-02-06 13:02:36 +0300

    created business and entitties layer

--- Workaround/Program.cs
+++ Workaround/Program.cs
@@ -8,7 +8,49 @@ namespace Workaround
         {
             Console.WriteLine("Hello World!");
 
+            SelamVer("engin");
+            SelamVer("ahmet");
+            SelamVer();
+
+            int sonuc = Topla(3, 5);
+            Topla();
+
+
+            //Diziler Arrays
+
+             
+
+
+
+
+
+
+
+
+
+
+
+            Vatandas vatandas1 = new Vatandas();
+
             Console.ReadLine();
         }
+        static void SelamVer(string isim = "isimsiz")
+        {
+            Console.WriteLine("Merhaba" + " " + isim);
+        }
+
+        static int Topla(int sayi1 = 5, int sayi2 = 7)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam : " + sonuc);
+            return sonuc;
+        }
+    }
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
     }
 }
