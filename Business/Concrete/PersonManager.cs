c0132baac8503d71381deb6221b6d1492c82d3ef
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-02-06 18:23:58 +0300

    end of the lesson

--- Business/Concrete/PersonManager.cs
+++ Business/Concrete/PersonManager.cs
@@ -1,4 +1,6 @@
-﻿using Entities.Concrete;
+﻿using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Business.Concrete
 {
-    public class PersonManager
+    public class PersonManager : IApplicantService
     {
         public void ApplyForMask(Person person)
         {
@@ -17,6 +19,14 @@ namespace Business.Concrete
         public List<Person> GetList()
         {
             return null;
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+
+            return client.TCKimlikNoDogrulaAsync(new TCKimlikNoDogrulaRequest(new TCKimlikNoDogrulaRequestBody(person.NationalIdentity, person.FirstName.ToUpper(), person.LasttName.ToUpper(), person.DateOfBirthYear))).Result.Body.TCKimlikNoDogrulaResult;
         }
     }
 }
--- Workaround/Program.cs
+++ Workaround/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
 
 namespace Workaround
 {
@@ -18,14 +21,50 @@ namespace Workaround
 
             //Diziler Arrays
 
-             
+
+            string ogrenci1 = "Engin";
+            string ogrenci2 = "Kerem";
+            string ogrenci3 = "Berkay";
+
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = ogrenci1;
+            ogrenciler[1] = ogrenci2;
+            ogrenciler[2] = ogrenci3;
+
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            string[] sehirler1 = new[] { "Ankara", "İstanbul", "İzmir" };
+            string[] sehirler2 = new[] { "Bursa", "Antalya", "Diyarbakır" };
+
+            sehirler2 = sehirler1;
+            sehirler1[0] = "Adana";
+            Console.WriteLine(sehirler2[0]);
+
+            foreach (string sehir in sehirler2) 
+            {
+                Console.WriteLine("foreach" + sehir);
+            }
+
+            //
+
+            List<string> yeniSehirler1 = new List<string> { "Ankara", "İstanbul", "İzmir" };
+            yeniSehirler1.Add("Adana");
+            foreach (var sehir in yeniSehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+            Person person1 = new Person();
+            person1.FirstName = "Yahya";
+            person1.LasttName = "Yağmur";
+            person1.DateOfBirthYear = 1983;
+            person1.NationalIdentity = 12345678910;
 
 
-
-
-
-
-
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
 
 
 
